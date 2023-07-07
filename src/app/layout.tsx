73e2ddbc3ec77a0dab02f5efb8760c64dfc00aae
sem oktavianus <sem.oktavianus@gmail.com>
2023-07-07 13:19:43 +0700

    basic navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-import "@/styles/globals.css"
+import Navbar from '@/components/navbar/Navbar'
+import { cn } from '@/lib/utils'
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
 
 export const metadata = {
-	title: "Sudut Pandang",
-	description: "Menumbuhkan Dialog, Merangkul Perspektif",
+	title: 'Sudut Pandang',
+	description: 'Menumbuhkan Dialog, Merangkul Perspektif',
 }
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
 	children,
@@ -11,8 +16,19 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html
+			lang='en'
+			className={cn(
+				'bg-white text-slate-900 antialiased light',
+				inter.className
+			)}
+		>
+			<body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+				<div className='container max-w-7xl mx-auto h-full pt-12'>
+					<Navbar />
+					{children}
+				</div>
+			</body>
 		</html>
 	)
 }
